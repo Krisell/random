074e5e5fc6683e2924b9fdc48151903410157ded
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-10-14 13:09:04 +0200

    Default argument

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ const Random = {
       return Array(number).fill(0).map(el => Math.floor(Math.random() * 10)).join('')
     },
 
-    randomString(length) {
-      let stringLength = length || 20 // Default length
+    randomString(length = 20) {
       let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-      return Array(stringLength)
+      return Array(length)
         .fill(0)
         .map(el => el = possible.charAt(Math.floor(Math.random() * possible.length)))
         .join('')
